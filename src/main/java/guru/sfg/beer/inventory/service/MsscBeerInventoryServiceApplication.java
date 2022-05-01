229f11985b5d33cd5b75498ac83c96d03b51c010
osva-developer <oswaldomoralesperez94@gmail.com>
2022-05-01 14:13:20 -0500

    CREATED:project inventory service was created

---
+++ src/main/java/guru/sfg/beer/inventory/service/MsscBeerInventoryServiceApplication.java
@@ -0,0 +1,14 @@
+package guru.sfg.beer.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+public class MsscBeerInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
@@ -0,0 +1,14 @@
+
+package guru.sfg.beer.inventory.service.repositories;
+
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
+
+    List<BeerInventory> findAllByBeerId(UUID beerId);
+}
