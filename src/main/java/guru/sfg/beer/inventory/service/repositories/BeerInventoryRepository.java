91d0722088d4cf69d5b42d80ccd5b8e7fdf31df1
osva-developer <oswaldomoralesperez94@gmail.com>
2022-05-05 13:01:59 -0500

    update: remove zero in hand inventory records #18

--- src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
+++ src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
@@ -11,4 +11,6 @@ import java.util.UUID;
 public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
 
     List<BeerInventory> findAllByBeerId(UUID beerId);
+    
+    List<BeerInventory> findAllByUpc(String upc);
 }
